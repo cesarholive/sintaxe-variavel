97d4f28f1dedefdf79d8851f7c9d277c200e714e
Cesar H Olivira <cesar_ho1993@hotmail.com>
2022-09-21 08:42:46 -0300

    boolean and if

---
+++ sintaxe-variavel-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,17 @@
+
+public class TestaCaracteres {
+
+		public static void main(String[] args) {
+			char letra = 'a';
+			System.out.println(letra);
+			
+			char valor = 66;
+			System.out.println(valor);
+			
+			valor = (char) (valor +1 ); //precisa ser feito desta forma, pois 1 é inteiro e não cabee dentro de char
+			System.out.println(valor);
+			
+			String palavra = "Alura cursos online de tecnologia";
+			System.out.println(palavra);
+		}
+}
